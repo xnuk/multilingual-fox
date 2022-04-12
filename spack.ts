75f601e3d4054a1256ff8f86fab7710fbce11293
Xnuk Shuman <admin@xnu.kr>
2022-04-13 04:59:34 +0900

    minify

--- spack.ts
+++ spack.ts
@@ -3,7 +3,7 @@ import { writeFile, readFile } from 'node:fs/promises';
 
 import { compileBundleOptions } from '@swc/core/spack.js';
 import type { OutputConfig } from '@swc/core/spack.js';
-import { bundle } from '@swc/core';
+import { bundle, minify } from '@swc/core';
 import JSZip from 'jszip';
 
 const resolve = (path: string) => fileURLToPath(new URL(path, import.meta.url));
@@ -37,7 +37,7 @@ const options = compileBundleOptions({
       paths: {
         'jsx/*': ['jsx/*'],
       },
-      target: 'es2021',
+      target: 'es2020',
     },
     module: { type: 'es6', strict: true, strictMode: true, noInterop: true },
   },
@@ -59,9 +59,24 @@ const main = async () => {
     (await execute(manifestCode.code)) as typeof import('./src/manifest')
   ).manifest;
 
+  const indexMinified = await minify(`(() => {${indexCode.code}})()`, {
+    compress: {
+      defaults: true,
+      conditionals: true,
+      ecma: 2020,
+      passes: 10,
+      inline: 3,
+      reduce_vars: true,
+      unsafe: true,
+      unsafe_arrows: true,
+      unsafe_math: true,
+    } as any,
+    mangle: true,
+  });
+
   const zip = new JSZip();
   zip.file('manifest.json', JSON.stringify(manifest));
-  zip.file('index.js', indexCode.code);
+  zip.file('index.js', indexMinified.code);
 
   for (const path of Object.values(manifest.icons)) {
     zip.file(path, await readFile(resolve('./' + path)));
